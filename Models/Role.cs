72380686cee4ee12e494d87ec6fe655e67fe860e
Iggy988 <igor.rodic1988@gmail.com>
2023-07-30 22:55:12 +0200

    Add project files.

---
+++ Models/Role.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models;
+
+public class Role
+{
+    public int RoleId { get; set; }
+    [MaxLength(30)]
+    public string RoleName { get; set; } = string.Empty;
+    [MaxLength(300)]
+    public string RoleDescription { get; set; } = string.Empty;
+    public ICollection<User>? Users { get; set; }
+}
