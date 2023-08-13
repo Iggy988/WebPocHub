fd6dadfca68ad43a007199c382a15d691efbb4e8
Iggy988 <igor.rodic1988@gmail.com>
2023-08-13 22:52:06 +0200

    implementing different return types (IActionResult, ActionResult)

---
+++ WebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,63 @@
+﻿using Dal;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EmployeeController : ControllerBase
+{
+    private readonly ICommonRepository<Employee> _employeeRepository;
+
+    public EmployeeController(ICommonRepository<Employee> repository)
+    {
+        _employeeRepository = repository;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<IEnumerable<Employee>> Get()
+    {
+        var employees = _employeeRepository.GetAll();
+        if (employees.Count <= 0)
+        {
+            return NotFound();
+        }
+        return Ok(employees);
+    }
+    [HttpGet("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<Employee> GetDetails(int id)
+    {
+
+        var employee = _employeeRepository.GetDetails(id);
+        return employee == null ? NotFound() : Ok(employee);
+    }
+
+    /*
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Employee>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    // public IEnumerable<Employee> Get()
+    public IActionResult Get()
+    {
+        var employees = _employeeRepository.GetAll();
+        if (employees.Count <= 0)
+        {
+            return NotFound();
+        }
+        return Ok(employees);
+    }
+    [HttpGet("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Employee))]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult GetDetails(int id) 
+    {
+
+        var employee = _employeeRepository.GetDetails(id);
+        return employee == null ? NotFound() : Ok(employee);
+    }*/
+}
---
+++ WebApi/Program.cs
@@ -0,0 +1,35 @@
+using Dal;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using WebPocHub.Dal;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<WebPocHubDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("MsSqlConStr"));
+});
+builder.Services.AddTransient<ICommonRepository<Employee>, CommonRepository<Employee>>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
