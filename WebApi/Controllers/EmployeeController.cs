ed4ef7d63cf3c327df2ac638960f70cd5d5691e2
Iggy988 <igor.rodic1988@gmail.com>
2023-08-16 23:50:24 +0200

    Adding CUD operations

--- WebApi/Controllers/EmployeeController.cs
+++ WebApi/Controllers/EmployeeController.cs
@@ -37,6 +37,57 @@ public class EmployeeController : ControllerBase
         return employee == null ? NotFound() : Ok(employee);
     }
 
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public ActionResult Create(Employee employee)
+    {
+        _employeeRepository.Insert(employee);
+        var result = _employeeRepository.SaveChanges();
+        if (result > 0)
+        {
+            //actionName - The name of the action to use for generating the URL
+            //routeValues - The route data to use for generating the URL
+            //value - The content value to format in the entity body
+            return CreatedAtAction("GetDetails", new { id = employee.EmplyeeId }, employee);
+        }
+        return BadRequest();
+    }
+
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public ActionResult Update(Employee employee)
+    {
+        _employeeRepository.Update(employee);
+        var result = _employeeRepository.SaveChanges();
+        if (result > 0)
+        {
+            return NoContent();
+        }
+        return BadRequest();
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<Employee> Delete(int id)
+    {
+        var employee = _employeeRepository.GetDetails(id);
+        if (employee == null)
+        {
+            return NotFound();
+        }
+        else
+        {
+            _employeeRepository.Delete(employee);
+            _employeeRepository.SaveChanges();
+            return NoContent();
+        }
+        
+    }
+
+
     /*
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Employee>))]
